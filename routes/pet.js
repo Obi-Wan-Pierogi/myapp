b8267b5214435016c4079419fb1a9d9d53d41757
Lee Houk <leehouk@gmail.com>
2023-10-01 18:47:03 -0700

    initial commit

---
+++ routes/pet.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var router = express.Router();
+
+
+
+// mock some data
+const pets = [
+    {
+        id: 1,
+        name: 'Fluffy',
+        breed: 'Siamese',
+        type: 'cat',
+        notes: [
+            'Fluffy loves to have her belly rubbed',
+            'Fluffy is really grumpy around other cats'
+        ]
+    },
+    {
+        id: 2,
+        name: 'Fido',
+        breed: 'Beagle',
+        type: 'dog',
+        notes: [
+            'Fido loves to be scratched behind the ears',
+            'Fido is really happy around other dogs'
+        ]
+    },
+    {
+        id: 3,
+        name: 'Goldberg',
+        breed: 'Goldfish',
+        type: 'fish',
+        notes: [
+            'Goldberg loves to swim around',
+            'Goldberg is eats alot'
+        ]
+
+    }
+]
+
+
+// GET: read all pets
+router.get('/', (req, res) => { 
+    //get all pets
+    res.statusCode = 200;
+    res.json(pets);
+});
+
+
+// GET: read a single pet
+router.get('/:id', (req, res) => { 
+    //get a single pet
+
+});
+
+// POST: create a new pet
+router.post('/', (req, res) => { 
+    //create a new pet
+
+});
+
+// PUT: update a single pet
+router.put('/:id', (req, res) => { 
+    //update a single pet
+
+});
+
+// DELETE: delete a single pet
+router.delete('/:id', (req, res) => { 
+    //delete a single pet
+
+});
+
+
+module.exports = router;
